05114b3a7e79d7ea9743a2e436182cfbf5311c20
root <pabiandrew77@gmail.com>
2023-02-16 06:04:03 -0800

    print something out using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main -the starting or entry point of the program.
  * Return:0, Non-zero value is concided an error
  */
-int main(viod)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
