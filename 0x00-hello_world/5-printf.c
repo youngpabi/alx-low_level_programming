23a30132fe2ea4c2fa9aebc47dfdbc97ff693e82
root <pabiandrew77@gmail.com>
2023-02-16 06:21:29 -0800

    usage of printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ * Return: 0, non-zero if there is an error
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
