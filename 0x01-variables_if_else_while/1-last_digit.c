edb478716107f1602384cc6d0c8d065d8ac3a71b
root <pabiandrew77@gmail.com>
2023-02-17 06:28:02 -0800

    remainder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
- * main -prints the remainder or last digit  of random number,showing >5,<6,=0
+ * main -prints last digit or modulus of random number,showing >5,<6,=0
  *
  * Return: 0, succesful
  */
 int main(void)
 {
 	int n;
-        int c;
+
 	/*code generates random number*/
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        c = n % 10;
-	if (c > 5)
+
+	if (n % 10  > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if (c == 0)
+	if (n % 10 == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	else (c < 6  && n % 10 != 0)
+	if (n % 10 < 6  && n % 10 != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
