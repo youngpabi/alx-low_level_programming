c4024edced6dd261ed88aec208036c175fc082ad
root <pabiandrew77@gmail.com>
2023-02-20 16:48:22 -0800

    100 comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry 
+ *
+ * Return: 0 (succesful)
+ */
+int main(void)
+{
+	/*
+	 * prints all combination of 2 different digits
+	 */
+
+	/* take x abd y as possible digits */
+	int x, y;
+
+	/*loop through x,1st digit,lowest of the 2, highest possible is 8*/
+	for (x = 0 ; x <= 8 ; x++)
+	{
+		/*loop through y,2nd digit,higher than a by 1,highest possible is 9*/
+		for (y = a + 1 ; y <= 9 ; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			/*continues to put coma,space,stop if last highest combination is met*/
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
