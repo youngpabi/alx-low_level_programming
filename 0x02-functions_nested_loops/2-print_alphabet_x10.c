5a153082fd59e181bb736a4e229640c3997f1117
root <pabiandrew77@gmail.com>
2023-02-21 11:12:16 -0800

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int king = 0;
+	int nextLine = 0;
 char s;
 
-	while (king <= 9)
+	while (nextLine <= 9)
 	{
 		for (s = 'a'; s <= 'z'; s++)
 		{
 			_putchar(s);
 		}
 		_putchar('\n');
-		king;
+		nextLine++;
 	}
 }
