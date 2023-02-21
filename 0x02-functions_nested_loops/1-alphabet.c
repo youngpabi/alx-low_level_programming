6c5ba80116d901b8d2e3399783946da7eca5d641
root <pabiandrew77@gmail.com>
2023-02-21 09:43:01 -0800

    dodn

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char y;
+
+	for (y = 'a'; key <= 'z'; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
