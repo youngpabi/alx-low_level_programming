14e3b756e14e45ea9cee1d8c5990b25b944c3d0b
root <pabiandrew77@gmail.com>
2023-02-21 12:02:58 -0800

    dwaf

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
-int _islower(int x)
+int _islower(int c)
 {
-	if (x >= 'a' && x <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
