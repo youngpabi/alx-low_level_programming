07d5fad420f737c0ffde6e0ff7180d4ec101a7b4
root <pabiandrew77@gmail.com>
2023-02-27 08:48:43 -0800

    key

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,17 @@
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
-{	
-       	int len, i;
-       	len = 0;
-       	while (src[len] != '\0')
+{
+	int len, i;
+	len=0;
+	while (scr[len] != '\0')
 	{
 		len++;
 	}
-	for (i = 0; i < len; i++)
+	for (i=0;i<len;i++)
 	{
-		dest[i] = src[i];
+		dest[i]=src[i];
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[i]='\0';
+	return(dest);
 }
