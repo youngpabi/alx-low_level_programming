45bf7d2ea193f1506fc93e62b3f713996465a0ae
Youngpabi <pabiandrew77@gmail.com>
2023-04-11 01:06:45 +0000

    soda

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,18 +32,18 @@ size_t _strlen(char *str)
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int we;
-	ssize_t len;
+	int wet;
+	ssize_t le;
 
 	if (filename == NULL)
 		return (-1);
-	we = open(filename, O_WRONLY | O_APPEND);
+	wet = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		len = write(we, text_content, _strlen(text_content));
-	close(we);
-	if (len == -1)
+		le = write(wet, text_content, _strlen(text_content));
+	close(wet);
+	if (le == -1)
 		return (-1);
 	return (1);
 }
