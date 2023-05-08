1ba2155c7f92d66a1e7442f20646557e9d581fd9
youngpabi <pabiandrew77@gmail.com>
2023-05-08 16:46:17 +0000

    gop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -68,9 +68,9 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	froms = open(argv[1], O_RDONLY);
+	rs = read(froms, buffer, 1024);
+	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (froms == -1 || rs == -1)
