b84ad0840a1b503c485f34504d28e7d18735a791
youngpabi <pabiandrew77@gmail.com>
2023-05-08 16:27:17 +0000

    gop

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,49 +1,34 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-/**
- * _strlen - the length of a string
- * @str: A pointer to string
- *
- * Return: the length of string
- */
-
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
 
 /**
  * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
  *
- * Return: -1 If  function fails or filename is NULL
- *         -1 If  file does not exist the user lacks write permissions
- *         Else 1.
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int wet;
-	ssize_t le;
+	int op, ws, lens = 0;
 
 	if (filename == NULL)
 		return (-1);
-	wet = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
-		le = write(wet, text_content, _strlen(text_content));
-	close(wet);
-	if (le == -1)
+	{
+		for (lens = 0; text_content[lens];)
+			lens++;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	ws = write(op, text_content, lens);
+
+	if (op == -1 || ws == -1)
 		return (-1);
+
+	close(op);
+
 	return (1);
 }
